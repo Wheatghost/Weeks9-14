5f90bfcd4dcbcce0de914e4045f0431c5bb055c8
miquelan <miquelan@OA-S141-11>
2026-03-25 11:35:51 -0400

    did more sound shenanigans

--- Assets/Scripts/Week 11/Knight.cs
+++ Assets/Scripts/Week 11/Knight.cs
@@ -1,9 +1,12 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Knight : MonoBehaviour
 {
     public AudioSource StepSfx;
     public AudioSource SwingSfx;
+    public List<AudioClip> clips;
+    public int randomSound;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,12 +16,13 @@ public class Knight : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        randomSound = (int)Random.Range(0,clips.Count);
     }
 
     public void Footstep()
     {
         //Debug.Log("Step");
+        StepSfx.clip = clips[randomSound];
         StepSfx.Play();
     }
 
